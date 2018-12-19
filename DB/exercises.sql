804982812e10032558b01a5b1d604c74aae5dc50
Braiden Miller <braidenjamesmiller@gmail.com>
2018-12-19 15:42:11 -0700

    made schema

---
+++ DB/exercises.sql
@@ -0,0 +1,53 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema exercisesdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `exercisesdb` ;
+
+-- -----------------------------------------------------
+-- Schema exercisesdb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `exercisesdb` DEFAULT CHARACTER SET utf8 ;
+USE `exercisesdb` ;
+
+-- -----------------------------------------------------
+-- Table `workout`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `workout` ;
+
+CREATE TABLE IF NOT EXISTS `workout` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(75) NOT NULL,
+  `category` VARCHAR(45) NOT NULL,
+  `Description` VARCHAR(150) NOT NULL,
+  `Sets` INT NOT NULL,
+  `reps` INT NOT NULL,
+  `rest` VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+SET SQL_MODE = '';
+DROP USER IF EXISTS exerciseuser@localhost;
+SET SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+CREATE USER 'exerciseuser'@'localhost' IDENTIFIED BY 'workout';
+
+GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'exerciseuser'@'localhost';
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `workout`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `exercisesdb`;
+INSERT INTO `workout` (`id`, `name`, `category`, `Description`, `Sets`, `reps`, `rest`) VALUES (1, 'curls', 'bicepts', 'grab a dumbell and lift it palms up while keeping your elbow in the same spot', 4, 10, '30');
+
+COMMIT;
+
